4718a36f7163b483f2a7945b3ee17ec4693eed84
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2026-06-09 11:44:27 -0300

    Customizing the PDF output

--- conf.py
+++ conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+import ast
+
 project = 'grs-doc'
 copyright = '2025, SpaceLab'
 author = 'SpaceLab'
@@ -24,6 +27,18 @@ bibtex_bibfiles = ['references.bib']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Identify the Sphinx builder being used
+if '-b' in sys.argv:
+    builder = sys.argv[sys.argv.index('-b') + 1]
+elif '-M' in sys.argv:
+    builder = sys.argv[sys.argv.index('-M') + 1]
+else:
+    builder = 'html'  # default builder
+
+# Exclude the PDF-specific index from the HTML build
+if builder in ['html', 'dirhtml']:
+    exclude_patterns.append('pdf-index.rst')
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -33,3 +48,31 @@ html_static_path = ['_static']
 # Navigation bar title
 html_title = "SpaceLab Ground Station"
 html_short_title = "SpaceLab Ground Station"
+
+# PDF output configuration
+latex_documents = [
+    (
+        'pdf-index',                        # Root document (e.g., 'index' or 'pdf-index')
+        'slb-grs-doc-' + release + '.tex',  # Output LaTeX file name (no spaces)
+        'SpaceLab Ground Station',          # Document title (can be empty to use the root doc's title)
+        'SpaceLab',                         # Author name(s).
+        'manual',                           # Document type: 'manual' or 'howto'
+        False,                              # toctree_only: if True, only include docs in toctree
+    ),
+]
+
+latex_logo = 'img/logo.jpg'
+
+latex_toplevel_sectioning = 'chapter'
+latex_show_pagerefs = True
+latex_show_urls = 'footnote'
+
+# Replace with the path to your local override file
+latex_elements_file = "_dev/latex_elements_custom.txt"
+
+latex_elements = dict()
+
+with open(latex_elements_file, "rt") as file:
+    latex_config = file.read()
+    if latex_elements == {}:
+      latex_elements = ast.literal_eval(latex_config)
---
+++ pdf-index.rst
@@ -0,0 +1,30 @@
+.. grs-doc documentation master file, created by
+   sphinx-quickstart on Thu June  9 10:59:34 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+=======================
+SpaceLab Ground Station
+=======================
+
+.. list-table::
+   :widths: 30 15 40 15
+   :header-rows: 1
+
+   * - **Author**
+     - **Version**
+     - **Comment**
+     - **Date**
+   * - G. M. Marcelino
+     - v0.1
+     - First release
+     - TBD
+
+.. toctree::
+   :maxdepth: 3
+
+   overview
+   hardware
+   software
+   ctrl-room
+   references
